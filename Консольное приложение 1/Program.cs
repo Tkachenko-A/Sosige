afa88c4b590c140d2afaf0ab10fc552a2e346d91
Tkachenko-A <107672442+Tkachenko-A@users.noreply.github.com>
2022-11-08 15:57:13 +0700

    Update Program.cs

--- Консольное приложение 1/Program.cs
+++ Консольное приложение 1/Program.cs
@@ -2,20 +2,27 @@
 using System;
 public class PrimeGenerator
 {
- private static int s;
  private static bool[] f;
- private static int[] primes;
+ private static int[] result;
  public static int[] GeneratePrimeNumbers(int maxValue)
  {
  if (maxValue < 2)
  return new int[0];
  else
  {
- InitializeSieve(maxValue);
- Sieve();
- LoadPrimes();
- return primes; // вернуть простые числа
+ InitializeArrayOfIntegers(maxValue);
+ CrossOutMultiples();
+ PutUncrossedIntegersIntoResult();
+ return result;
  }
+ }
+ private static void InitializeArrayOfIntegers(int maxValue)
+ {
+ // объявления
+ f = new bool[maxValue + 1];
+ f[0] = f[1] = false; //не простые числа и не кратные
+ for (int i = 2; i < f.Length; i++)
+ f[i] = true;
  }
  private static void LoadPrimes()
  {
